c6ca3e55803c8fef7a7119bf43e88452f72f9ee6
octa.quintero <octa.quinteroo@gmail.com>
2024-06-28 21:23:39 -0300

    typeorm

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,19 +1,21 @@
 import { DataSource } from "typeorm"
 import { config as dotenvConfig } from 'dotenv';
+import Usuarios from "src/entities/usuario";
+
 
 dotenvConfig({ path: '.env' });
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST,
     port: 5432,
+    host: process.env.DB_HOST,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    dropSchema: true, // <= esta propiedad borra toda la DB si esta activado
+    dropSchema: true,
     synchronize: true,
-    logging: false, // ["error"], <= solo muestre errores de la DB
-    entities: [],
+    logging: false,
+    entities: [ Usuarios ],
     subscribers: [],
     migrations: [],
 })
