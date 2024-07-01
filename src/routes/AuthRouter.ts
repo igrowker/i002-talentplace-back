cae1ef51685c490960a276aaa340596cf71013e4
Cesar Ausa <cesarausaprog@gmail.com>
2024-07-01 20:45:39 -0300

    clean trash

--- src/routes/AuthRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import AuthController from "../controllers/Auth.Controller";
-
-const authRouter: Router = Router();
-
-authRouter.post("/2fa/setup", AuthController.postAuth2FaSetup);
-authRouter.post("/2fa/verify", AuthController.postAuth2FaVerify);
-
-export default authRouter;
