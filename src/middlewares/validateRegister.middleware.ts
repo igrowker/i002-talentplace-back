403af5fe4c0c68e09f07547fec19504bc619fe0b
octa.quintero <octa.quinteroo@gmail.com>
2024-07-17 09:22:53 -0300

    Fix validateRegister: specialCharRegex

--- src/middlewares/validateRegister.middleware.ts
+++ src/middlewares/validateRegister.middleware.ts
@@ -14,6 +14,29 @@ const validateRegisterData = (req: Request, res: Response, next: NextFunction) =
     throw { message: 'Formato de correo electrónico no válido', code: 400 };
   }
 
+    const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
+
+    // Verificar que no haya caracteres especiales en nombre, apellido, telefono, pais y tipo
+    if (specialCharRegex.test(nombre)) {
+        throw { message: 'El nombre no debe contener caracteres especiales.', code: 400 };
+    }
+
+    if (specialCharRegex.test(apellido)) {
+        throw { message: 'El apellido no debe contener caracteres especiales.', code: 400 };
+    }
+
+    if (specialCharRegex.test(telefono)) {
+        throw { message: 'El teléfono no debe contener caracteres especiales.', code: 400 };
+    }
+
+    if (specialCharRegex.test(pais)) {
+        throw { message: 'El país no debe contener caracteres especiales.', code: 400 };
+    }
+
+    if (specialCharRegex.test(tipo)) {
+        throw { message: 'El tipo no debe contener caracteres especiales.', code: 400 };
+    }
+
   // Función para validar la fortaleza de la contraseña
   const validatePasswordStrength = (password: string): boolean => {
     const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
