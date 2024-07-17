f8f12397f27e1a997becb4592aab27e9309fe92a
Cesar Ausa <cesarausaprog@gmail.com>
2024-07-17 01:48:00 -0300

    preload Data in projects implemented

--- src/helpers/preloadData.helper.ts
+++ src/helpers/preloadData.helper.ts
@@ -3,15 +3,24 @@ import { AppDataSource } from "../config/typeorm.config";
 import * as bcrypt from "bcryptjs";
 import Usuario from "../entities/usuario";
 import { preloadUsers } from "./usersData.helper";
+import Proyecto from "../entities/proyecto";
+import { preloadProjects } from "./projectsData.helper";
+import Categoria from "../entities/categoria";
+import { Habilidad } from "../entities/habilidad";
+import habilityService from "../services/hability.service";
+import categoryService from "../services/category.service";
 
 const UserRepository = AppDataSource.getRepository(Usuario);
+const ProjectRepository = AppDataSource.getRepository(Proyecto);
 
 export const preloadUsersData = async () => {
     await AppDataSource.manager.transaction(async (transactionalEntityManager) => {
 
         const users = await UserRepository.find();
         
-        if(users.length) return console.log(`No se hizo precarga de datos porque ya hay ${users.length} datos cargados`);
+        if(users.length)
+            return console.log(`No se hizo precarga de usuarios porque ya hay ${users.length} usuarios cargados`);            
+
         // users
         for await(const user of preloadUsers) {
             const newUser = await UserRepository.create({
@@ -20,6 +29,37 @@ export const preloadUsersData = async () => {
             });
             await transactionalEntityManager.save(newUser);
         }
+
         console.log("Precarga de Usuarios del Preload realizada con éxito");        
+
+    })
+
+    await AppDataSource.manager.transaction(async (transactionalEntityManager) => {
+
+        const projects = await ProjectRepository.find();
+        //user con rol empresa
+        const userCompany = await UserRepository.findOneBy({email: "nicoausa@gmail.com"});        
+        
+        if (projects.length > 5)
+            return console.log(`No se hizo precarga de proyectos porque ya hay ${projects.length} proyectos cargados`);            
+        
+        //proyectos
+        for await(const project of preloadProjects) {
+
+            //agrego categoria
+            const category: Categoria = await categoryService.postNewCategory(project.categoria);
+            //agrego habilidades
+            const habilities: Habilidad[] = await habilityService.postNewHability(project.habilidades);
+            const newProject = await ProjectRepository.create({
+                ...project,
+                empresaId: userCompany.id,
+                categoria: category,
+                habilidades: habilities
+            });
+            await transactionalEntityManager.save(newProject);
+        }
+
+        console.log("Precarga de Proyectos del Preload realizada con éxito");        
+        
     })
 }
---
+++ src/helpers/projectsData.helper.ts
@@ -0,0 +1,103 @@
+import IProject from "../interfaces/IProject.interface";
+
+//projects preload
+export const preloadProjects: IProject[] = [
+    {
+        titulo: "Desarrollo de una Aplicación de Gestión de Proyectos",
+        descripcion: "Una empresa de consultoría en gestión de proyectos busca un desarrollador freelance para crear una aplicación de gestión integral de proyectos. El proyecto incluye la creación de herramientas para planificación, seguimiento de tareas, gestión de recursos y generación de informes.",
+        requisitos: "Conocimientos en gestión de proyectos, habilidades en programación (preferiblemente en Java o C#), experiencia en desarrollo de software de gestión empresarial.",
+        habilidades: [
+            "liderazgo",
+            "gestión del cambio",
+            "resolución de conflictos",
+            "análisis de riesgos"
+        ],
+        categoria: "gestión empresarial",
+        modalidad: "presencial",
+        estado: true
+    },
+    {
+        titulo: "Desarrollo de una Plataforma Educativa Online",
+        descripcion: "Una organización educativa busca un desarrollador freelance para crear una plataforma educativa en línea. El proyecto incluye la implementación de cursos interactivos, herramientas de evaluación y seguimiento del progreso del estudiante.",
+        requisitos: "Conocimientos en educación online, habilidades en programación (preferiblemente en PHP o Python), experiencia en desarrollo de plataformas educativas.",
+        habilidades: [
+            "adaptabilidad pedagógica",
+            "diseño instruccional",
+            "gestión del aprendizaje",
+            "colaboración"
+        ],
+        categoria: "educación y tecnología",
+        modalidad: "remoto",
+        estado: true
+    },
+    {
+        titulo: "Desarrollo de un Portal de Noticias Interactivo",
+        descripcion: "Una agencia de medios está buscando un desarrollador freelance para crear un portal de noticias interactivo. El proyecto incluye la integración de actualizaciones en tiempo real, la creación de un sistema de comentarios y la implementación de funciones de personalización de contenido.",
+        requisitos: "Conocimientos en periodismo digital, habilidades en programación (preferiblemente en React o Angular), experiencia en desarrollo de portales web.",
+        habilidades: [
+            "creación de contenido",
+            "optimización de rendimiento",
+            "atención al usuario",
+            "creatividad"
+        ],
+        categoria: "tecnología y medios",
+        modalidad: "hibrido",
+        estado: true
+    },
+    {
+        titulo: "Desarrollo de un Sistema de Gestión de Recursos Humanos",
+        descripcion: "Una empresa de tecnología está buscando un desarrollador freelance para crear un sistema de gestión de recursos humanos. El proyecto incluye la implementación de módulos para la gestión de empleados, nómina, seguimiento de asistencia y evaluación del desempeño.",
+        requisitos: "Conocimientos en recursos humanos, habilidades en programación (preferiblemente en Ruby o C#), experiencia en desarrollo de sistemas empresariales.",
+        habilidades: [
+            "planificación",
+            "gestión de proyectos",
+            "comunicación clara",
+            "pensamiento estratégico"
+        ],
+        categoria: "tecnología y recursos humanos",
+        modalidad: "remoto",
+        estado: true
+    },
+    {
+        titulo: "Desarrollo de una Aplicación Móvil de Salud",
+        descripcion: "Una startup en el sector de la salud está buscando un desarrollador freelance para crear una aplicación móvil que ayude a los usuarios a realizar un seguimiento de su estado físico y bienestar. El proyecto incluye la implementación de funciones de seguimiento de actividad, recordatorios de medicación y conexión con dispositivos de salud.",
+        requisitos: "Conocimientos en desarrollo de aplicaciones móviles, habilidades en programación (preferiblemente en Swift o Kotlin), experiencia en proyectos relacionados con la salud.",
+        habilidades: [
+            "innovación",
+            "capacidad de análisis",
+            "orientación al usuario",
+            "colaboración"
+        ],
+        categoria: "tecnología y salud",
+        modalidad: "hibrido",
+        estado: true
+    },
+    {
+        titulo: "Implementación de una Plataforma de E-commerce",
+        descripcion: "Una empresa de comercio electrónico busca un desarrollador freelance para crear una plataforma de venta en línea. El proyecto incluye el desarrollo de un carrito de compras, la integración con pasarelas de pago y la implementación de funcionalidades de gestión de inventario.",
+        requisitos: "Conocimientos en comercio electrónico, habilidades en programación (preferiblemente en JavaScript o PHP), experiencia en desarrollo de plataformas web.",
+        habilidades: [
+            "creatividad",
+            "adaptabilidad",
+            "trabajo en equipo",
+            "comunicación efectiva"
+        ],
+        categoria: "tecnología y comercio electrónico",
+        modalidad: "presencial",
+        estado: true
+    },
+    {
+        titulo: "Desarrollo de un Juego de Casino Online",
+        descripcion: "Una empresa de entretenimiento está buscando un desarrollador para crear un juego de casino online. El proyecto incluye el desarrollo de juegos de azar como póker, blackjack y tragamonedas, con integración de gráficos de alta calidad y mecanismos de pago seguros.",
+        requisitos: "Conocimientos en desarrollo de juegos, habilidades en programación (preferiblemente en JavaScript o C++), experiencia en integración de gráficos y mecanismos de pago.",
+        habilidades: [
+            "creatividad",
+            "resolución de problemas",
+            "atención al detalle",
+            "gestión del tiempo"
+        ],
+        categoria: "entretenimiento y juegos",
+        modalidad: "hibrido",
+        estado: true
+    }  
+]
