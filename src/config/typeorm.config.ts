75d4562f0ab3da3f44c08c5a7b724ec7e0bf239c
Cesar Ausa <cesarausaprog@gmail.com>
2024-07-26 14:50:22 -0300

    dropschema

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -18,7 +18,7 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_DATABASE,
-    dropSchema: true,
+    //dropSchema: true,
     synchronize: true,
     logging: false,
     entities: [ Usuario , Aplicacion , Comentario , Pago , Proyecto, Habilidad, Categoria ],
